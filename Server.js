a1278df2079e902e618442bac50deeb8297d6bca
JJsowndaryaswetha11 <nirmalajeyaseeli@gmail.com>
2024-12-16 17:02:16 +0530

    first commit2

--- Server.js
+++ Server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 
 const app = express();
-const PORT = 5000;
+
 
 // Middleware
 app.use(cors());
@@ -127,6 +127,6 @@ app.post('/api/register', async (req, res) => {
   
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running`);
 });
